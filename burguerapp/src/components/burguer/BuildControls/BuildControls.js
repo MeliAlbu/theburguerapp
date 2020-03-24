824af2ca4f8c8a8840104f93c96ac8ee158e5197
melialbu <melialbu@gmail.com>
2020-03-24 03:35:14 -0300

    checkout modal working - starting backdrop

--- burguerapp/src/components/burguer/BuildControls/BuildControls.js
+++ burguerapp/src/components/burguer/BuildControls/BuildControls.js
@@ -12,12 +12,18 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={"BuildControls"}>
+        <p>Current Price:<strong> {props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
-                added={() => props.ingredientAdded(ctrl.type)} /> //esto esta llamando al handler, recibe la props que se mando de burguer builder
+                added={() => props.ingredientAdded(ctrl.type)} 
+                removed= {() => props.ingredientRemoved(ctrl.type)} //esto esta llamando al handler, recibe la props que se mando de burguer builder
+                disabled={props.disabled[ctrl.type]}  />
         ))}
+        <button className={"OrderButton"}
+        disabled={!props.purchasable}
+        onClick={props.ordered}>ORDER NOW!</button>
 
     </div>
 
